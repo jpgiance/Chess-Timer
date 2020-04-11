5d97d9a204c9f5504590f32e8d298d797129d1aa
jorgepg <jpgiance@gmail.com>
2020-04-11 00:35:07 -0500

    feat: Added design changes to GameActivity

--- app/src/main/java/com/jorgegiance/chess_timer/ui/GameActivity.java
+++ app/src/main/java/com/jorgegiance/chess_timer/ui/GameActivity.java
@@ -94,7 +94,7 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
+        mControlsView = findViewById(R.id.button_layer);
         mContentView = findViewById(R.id.fullscreen_content);
 
 
@@ -109,7 +109,7 @@ public class GameActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+       // findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
